3a4d621b0b978fb0261dc1bde5e1a5fafa1fb1a7
YASMIN TARGINO DE ALEXANDRE <23111771@CFP127ALUNO.LOCAL>
2023-12-06 12:25:19 -0300

    javascript2

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
 'use strict'
 
-const accessKey = 'oQmD9vxtQWOEJVAbPpn9bcMft2ts0rJYI8QdnLtLaaY'
+const searchImages = async(text) => {
+    const key = 'live_Iol31MDJPlIbTExY1LyOcqQoJmQ5wTk7SD01jZjWbyjmpnkNc07eKp2OgIiBy2Rt';
+    const url = `https://cdn2.thecatapi.com/images/ebv.jpg/?key=${key}&q=${text}`;
+    const response = await fetch(url);
+    return response.json();
+};
+
+const accessKey = "oQmD9vxtQWOEJVAbPpn9bcMft2ts0rJYI8QdnLtLaaY"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("pesquisa")
+const containerImg = document.querySelector(".container-imagens")
+const botaoMostrar = document.getElementById(".botao-mostrar-mais")
